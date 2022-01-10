032dcd29dc847ffcd0f1d1e1745bb59f37867db6
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-10 09:09:33 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ async function post_visitor() {
     let response = await fetch('https://rff8fll6zc.execute-api.us-east-1.amazonaws.com/Prod/transactions?transactionId=5&type=PURCHASE&amount=500', {
       method: 'POST',
       headers: {
-        'Content-Type', 'application/json'
+        //'x-api-key': 'JslbDfdt1F8fl7wE4CRIj1Oqidmtmzqw4lZ539Sj',
+        'Content-Type': 'application/json',
       }
     });
     let data = await response.json()
@@ -34,7 +35,9 @@ async function get_visitors() {
     let response = await fetch('https://rff8fll6zc.execute-api.us-east-1.amazonaws.com/Prod/transactions?transactionId=5&type=PURCHASE&amount=500', {
       method: 'GET',
       headers: {
-        'Content-Type', 'application/json'
+        //'x-api-key': 'JslbDfdt1F8fl7wE4CRIj1Oqidmtmzqw4lZ539Sj',
+        'Content-Type': 'application/json',
+
       }
     });
     let data = await response.json()
