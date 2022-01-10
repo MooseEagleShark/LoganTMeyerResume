ef8bb3ca0a494a7b9642e264724f626d6268f7c8
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-10 10:41:41 -0700

    Update visitorcount.js

--- js/visitorcount.js
+++ js/visitorcount.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const zooming = new Zooming({
+    bgColor: 'rgb(0, 0, 0)',
+    bgOpacity: 0.8,
+    customSize: '100%'
+  })
+
+  zooming.listen('.img-zoomable')
+})
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
