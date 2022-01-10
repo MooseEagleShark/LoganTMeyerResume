070e78c0afe9bb2ca9d7c805f06f9a1d21b04c34
Logan Meyer <loganmeyer@Logans-Mac-Pro.local>
2022-01-10 10:28:27 -0700

    1 works

--- js/visitorcount.js
+++ js/visitorcount.js
@@ -68,6 +68,20 @@ xhttp2.setRequestHeader('Content-Type', 'application/json');
 
 xhttp2.send(); // GET
 
+var xhttp3 = new XMLHttpRequest();
+xhttp3.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // Typical action to be performed when the document is ready:
+    document.getElementById("transactions3").innerHTML = xhttp.responseText;
+  }
+};
+xhttp3.open(
+  "GET",
+  "https://rff8fll6zc.execute-api.us-east-1.amazonaws.com/Prod/transactions?transactionid=5&type=PURCHASE&amount=500",
+  true
+);
+xhttp3.send();
+
 // xhttp.send(string)  // POST
 //However, always use POST requests when:
 
